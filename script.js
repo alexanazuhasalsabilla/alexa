5ab274f21edf8ed96a4a3b52ca23acdc1f65a163
alexanazuhasalsabilla <alexanazuhasalsabilla@gmail.com>
2025-10-09 12:01:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1. Pilih tombol berdasarkan ID-nya
-const tombolSapa = document.getElementById('sapa Alexa!');
+const tombolSapa = document.getElementById('Sapa Alexa!');
 // 2. Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click', function() {
   // 3. Jalankan kode ini ketika tombol di-klik
